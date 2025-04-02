a70a3007525fd07c3183be655c7948d5bcde0216
Magnus <198209962+MagnusHSundby@users.noreply.github.com>
2025-04-02 09:26:45 +0200

    smarting

--- index.js
+++ index.js
@@ -11,13 +11,10 @@ const client = new Client({
   ]
 });
 
-// logs message to console when bot is ready
 client.once('ready', () => {
   console.log('Bot is ready!');
 });
 
-// Set up command handlers
 setupCommandHandlers(client);
 
-// Login to Discord with your client's token
 client.login(process.env.DISCORD_BOT_ID);
